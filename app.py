5ca9f5830984eed8660ea7706c9523b3d2f25114
Stew <stephanbelizaire@gmail.com>
2020-07-30 00:27:47 -0400

    Some parts work
    d

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import MySQLdb.cursors
 from classes.book import Book
 from classes.author import Author
 from classes.bookSearch import bookSearch
-
+from classes.user import User
+from classes.rating import Rating
+from classes.rater import Rater
 
 app = Flask(__name__)
 
 app.secret_key = 'admin'
 
 # Database connection details
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_HOST'] = 'geektext.co6gooerckmv.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'TXwUJ8cGvDXjdVVPXSsl'
 app.config['MYSQL_DB'] = 'geektextdb'
 
 # initialize MySQL
@@ -49,14 +51,22 @@ def feature3():
 def feature4():
     return "Feature 4"
 
-@app.route('/feature5')
+@app.route('/feature5', methods=['GET','POST'])
 def feature5():
-    return render_template("feature5.html")
+
+    rater = Rater(mysql)
+    allOpinions = rater.displayOpinion()
+    allOrderOpinions = rater.orderReviews()
+    return render_template("feature5.html",allOpinions = allOpinions, allOrderOpinions = allOrderOpinions)
+
+@app.route('/newReview', methods=['GET'])
+def newReview():
+    rater = Rater(mysql)
+    return render_template("newReview.html")
 
 @app.route('/feature6')
 def feature6():
     return "Feature 6"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
